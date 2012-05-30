a99c6cdfd44523c31bf8bee5044f0e8c6f2b42ef
Kazato Sugimoto <uiureo@gmail.com>
2012-05-30 17:14:10 +0900

    動きを加えた

--- emit/canvas.js
+++ emit/canvas.js
@@ -10,9 +10,14 @@ onload = function () {
   setInterval(draw, 100);
 };
 
+Array.prototype.sample = function () {
+  return this[Math.floor(Math.random()*this.length)];
+};
+
 var draw = (function () {
   var T = 100;
-  var r = W > H ? W : H;
+  var r = W > H ? 2*W : 2*H;
+  var center = {x: W/2, y: H/2, dx: 10, dy: 10};
 
   return function() {
     // reset
@@ -20,7 +25,14 @@ var draw = (function () {
     ctx.fillRect(0, 0, W, H);
     ctx.save();
 
-    ctx.translate(W/2, H/2);
+    ctx.translate(center.x, center.y);
+    center.x += center.dx;
+    center.y += center.dy;
+
+    if (!(0 < center.x && center.x < W))
+      center.dx *= -1;
+    if (!(0 < center.y && center.y < H))
+      center.dy *= -1;
 
     for (i = 0; i < T; i++) {
       ctx.fillStyle = "rgba(" + [null,null,null].map(function(){return Math.floor(255*Math.random())}).join(',') + ",1.0)";
@@ -33,6 +45,13 @@ var draw = (function () {
       ctx.fill();
       ctx.stroke();
     }
+
+    ctx.beginPath();
+    ctx.fillStyle = 'rgba(255,255,0,0.7)';
+    ctx.arc(0,0,10,0,2*Math.PI,false);
+    ctx.fill();
+    ctx.closePath();
+    
 
     ctx.restore();
   }
